9d466750b10a6d978e763d0d94ff7ff8385ec523
wikinfo <ynland@126.com>
2018-02-28 09:29:33 +0800

    图片拼接
    通过文件下的子文件夹【三家目录：根文件夹>子文件夹>.jpg格式图片文件 】将每个文件夹下面图片拼接成一张jpg,或者一个pdf文件。
    本工具做的不是很智能，是应一同学需要拼接上万文件内图片开发，喜欢的可以改改自用，

---
+++ imageConvertjpg_Pdf/imageConvert/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace imageConvert
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.txtFrompath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtToPath = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.process1 = new System.Diagnostics.Process();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(174, 95);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "开始转换";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(24, 14);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(75, 23);
+            this.btnSelect.TabIndex = 1;
+            this.btnSelect.Text = "选择路径";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // txtFrompath
+            // 
+            this.txtFrompath.Enabled = false;
+            this.txtFrompath.Location = new System.Drawing.Point(105, 14);
+            this.txtFrompath.Name = "txtFrompath";
+            this.txtFrompath.Size = new System.Drawing.Size(272, 21);
+            this.txtFrompath.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "输出路径：";
+            // 
+            // txtToPath
+            // 
+            this.txtToPath.Location = new System.Drawing.Point(105, 57);
+            this.txtToPath.Name = "txtToPath";
+            this.txtToPath.Size = new System.Drawing.Size(272, 21);
+            this.txtToPath.TabIndex = 4;
+            this.txtToPath.Text = "D:\\toPath";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // process1
+            // 
+            this.process1.StartInfo.Domain = "";
+            this.process1.StartInfo.LoadUserProfile = false;
+            this.process1.StartInfo.Password = null;
+            this.process1.StartInfo.StandardErrorEncoding = null;
+            this.process1.StartInfo.StandardOutputEncoding = null;
+            this.process1.StartInfo.UserName = "";
+            this.process1.SynchronizingObject = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(586, 262);
+            this.Controls.Add(this.txtToPath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFrompath);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.TextBox txtFrompath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtToPath;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Diagnostics.Process process1;
+    }
+}
+
---
+++ imageConvertjpg_Pdf/imageConvert/Form1.cs
@@ -0,0 +1,275 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace imageConvert
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string selectPath = "";
+        //固定高宽，便于后续处理
+        int WWidth = 1650;
+        int HHeight = 2320;
+        List<System.Drawing.Image> AllName = new List<System.Drawing.Image>();
+        /// <summary>
+        /// 选择路径
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择文件路径";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string foldPath = dialog.SelectedPath;
+                txtFrompath.Text = foldPath;
+                selectPath = foldPath;
+            }
+
+
+
+        }
+        //开始转换
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = false;
+            Thread thred = new Thread(Start);
+            thred.Start();
+            // btnStart.Enabled = true;
+        }
+        public void Start()
+        {
+            //获取文件信息
+            DirectoryInfo folder = new DirectoryInfo(selectPath);
+            int count = folder.GetDirectories().Count();
+            int current = 1;
+            string mess = DateTime.Now.ToString() + ":" + "本初一共处理数据，" + count + "条\r\n";
+            TextWrite(txtToPath.Text, DateTime.Now.ToString("yyyy-MM-dd"), mess);
+            foreach (var dirInfo in folder.GetDirectories())
+            {
+
+                Object thisLock = new Object();
+                lock (thisLock)
+                {
+                    GC.Collect();
+                    CombineImages(dirInfo.GetFiles("*.jpg"), dirInfo.Name, txtToPath.Text);
+                    GC.Collect();
+
+                }
+                current++;
+                TextWrite(txtToPath.Text, DateTime.Now.ToString("yyyy-MM-dd"), mess + "当前正在处理第" + current + "条，文件名称为，" + dirInfo.Name + "\r\n");
+
+
+            }
+        }
+
+
+
+
+
+
+        //合并图片
+        private void CombineImages(FileInfo[] files, string fileName, string toPath, ImageMergeOrientation mergeType = ImageMergeOrientation.Vertical, ConvertType convertType = ConvertType.PDF)
+        {
+            var finalImage = toPath;
+            var imgs = files.Select(f => System.Drawing.Image.FromFile(f.FullName));
+
+            var finalWidth = mergeType == ImageMergeOrientation.Horizontal ?
+                imgs.Sum(img => img.Width) :
+                imgs.Max(img => img.Width);
+
+            var finalHeight = mergeType == ImageMergeOrientation.Vertical ?
+                imgs.Sum(img => img.Height) :
+                imgs.Max(img => img.Height);
+
+            var finalImg = new Bitmap(finalWidth, finalHeight - (files.Count() * 1000));
+
+            switch (convertType)
+            {
+                #region JPG合成
+                case ConvertType.JPG:
+                    Graphics g = Graphics.FromImage(finalImg);
+                    g.Clear(SystemColors.AppWorkspace);
+
+                    var width = finalWidth;
+                    var height = finalHeight;
+                    var nIndex = 0;
+                    foreach (FileInfo file in files)
+                    {
+                        System.Drawing.Image img = System.Drawing.Image.FromFile(file.FullName);
+                        if (nIndex == 0)
+                        {
+                            g.DrawImage(img, new Point(500, 0));
+                            nIndex++;
+                            width = img.Width;
+                            height = img.Height;
+                        }
+                        else
+                        {
+                            switch (mergeType)
+                            {
+                                case ImageMergeOrientation.Horizontal:
+                                    g.DrawImage(img, new Point(width, 0));
+                                    width += img.Width;
+                                    break;
+                                case ImageMergeOrientation.Vertical:
+                                    g.DrawImage(img, new Point(500, height - 1000));
+                                    height += img.Height - 1000;
+                                    break;
+                                default:
+                                    throw new ArgumentOutOfRangeException("mergeType");
+                            }
+                        }
+                        img.Dispose();
+                    }
+                    string imageFilePath = finalImage + "\\" + fileName + ".jpg";
+                    g.Dispose();
+                    finalImg.Save(imageFilePath);
+                    finalImg.Dispose();
+                    break;
+                #endregion
+                #region PDF合成
+                case ConvertType.PDF:
+                    Document document = new Document();
+                    document.SetPageSize(new iTextSharp.text.Rectangle(WWidth + 72f, HHeight + 72f));
+                    PdfWriter write = PdfWriter.GetInstance(document, new FileStream(finalImage + "\\" + fileName + ".pdf", FileMode.OpenOrCreate, FileAccess.Write));
+                    document.Open();
+                    iTextSharp.text.Image jpg;
+
+                    foreach (FileInfo file in files)
+                    {
+                        System.Drawing.Image img = System.Drawing.Image.FromFile(file.FullName);
+                        jpg = iTextSharp.text.Image.GetInstance(img, ImageFormat.Jpeg);
+                        document.NewPage();
+                        document.Add(jpg);
+                    }
+                    if (document != null && document.IsOpen())
+                    {
+                        document.Close();
+                    }
+                    if (write != null)
+                    {
+                        write.Close();
+                    }
+                    break;
+                #endregion
+                default:
+                    break;
+            }
+
+
+        }
+        public void TurnTheImageToPdf(string FileName, FileInfo[] images)
+        {
+            //  AllName.Clear();
+            //  ChangeTheImageToS(ref SourceImage);
+            Document document = new Document();
+            document.SetPageSize(new iTextSharp.text.Rectangle(WWidth + 72f, HHeight + 72f));
+            PdfWriter write = PdfWriter.GetInstance(document, new FileStream(FileName, FileMode.OpenOrCreate, FileAccess.Write));
+            document.Open();
+            iTextSharp.text.Image jpg;
+
+            for (int i = 0; i < AllName.Count; ++i)
+            {
+                jpg = iTextSharp.text.Image.GetInstance(AllName[i], ImageFormat.Jpeg);
+                document.NewPage();
+                document.Add(jpg);
+            }
+            if (document != null && document.IsOpen())
+            {
+                document.Close();
+            }
+            if (write != null)
+            {
+                write.Close();
+            }
+        }
+        private void ChangeTheImageToS(ref List<string> ImageName)
+        {
+            for (int i = 0; i < ImageName.Count; ++i)
+            {
+                Bitmap src = new Bitmap(ImageName[i]);
+                Bitmap bmImage = new Bitmap(WWidth, HHeight);
+                Graphics g = Graphics.FromImage(bmImage);
+                g.InterpolationMode = InterpolationMode.Low;
+                g.DrawImage(src, new System.Drawing.Rectangle(0, 0, bmImage.Width, bmImage.Height), new System.Drawing.Rectangle(0, 0, src.Width, src.Height), GraphicsUnit.Pixel);
+                g.Dispose();
+                AllName.Add(bmImage);
+            }
+            GC.Collect();
+        }
+
+        /// <summary>
+        /// 写文本文件，换行，不换行，以流的方式写数据
+        /// </summary>
+        public void TextWrite(string str, string logName, string strins)
+        {
+
+            if (Directory.Exists(txtToPath.Text) == false) //如果不存在就创建file文件夹
+            {
+                Directory.CreateDirectory(str);
+            }
+
+                //判断文件的存在
+                if (File.Exists(str + "\\" + logName + ".log"))
+                {
+                    Console.WriteLine("该文件已经存在");
+                }
+                else
+                {
+                    //如果文件不存在，则创建该文件
+                    File.Create(str + "\\" + logName + ".log");
+                }
+
+                //写入字符串数组，并且换行
+                List<string> lstStr = new List<string>();
+                lstStr.Add(strins);
+                //如果该文件存在，并向其中追加写入数据
+                if (File.Exists(str + "\\" + logName + ".log"))
+                {
+                    File.AppendAllLines(str + "\\" + logName + ".log", lstStr, Encoding.UTF8);
+              
+                }
+                else
+                //如果该文件不存在，则创建该文件，写入数据
+                {
+                    //如果该文件存在，这个方法会覆盖该文件中的内容
+                    File.AppendAllLines(str + "\\" + logName + ".log", lstStr, Encoding.UTF8);
+                }
+                ////如果文件不存在，则创建；存在则覆盖
+
+                //System.IO.File.AppendAllLines(str + "\\" + logName + ".log", lstStr, Encoding.UTF8);
+                lstStr.Clear();
+            }
+        
+
+    }
+
+    enum ImageMergeOrientation
+    {
+        Horizontal,
+        Vertical,
+    }
+    enum ConvertType
+    {
+        JPG,
+        PDF
+    }
+}
